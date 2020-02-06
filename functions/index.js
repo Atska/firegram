86fa91de06bf8ee1a5d5c2517c58a54984b5d94b
[Atska] <Binh.Nguyen93@web.de>
2020-02-07 00:15:56 +0100

    Created a registration and a basic login. Auth of current user missing. Dont know if errors such as empty email or empty handle are checked.

--- functions/index.js
+++ functions/index.js
@@ -1,24 +1,39 @@
-// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require("firebase-functions");
-// The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require("firebase-admin");
-const express = require("express");
-// Initialize on Cloud Functions
-admin.initializeApp(functions.config().firebase);
-let db = admin.firestore();
+const app = require("express")();
+const firebase = require("firebase");
 
-const app = express();
+// Initialize Admin SDK
+const serviceAccount = require("./credentials.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://sirenesongapp.firebaseio.com"
+});
+const db = admin.firestore();
 
+/** project configs (unique, but no secret) from the project settings. May have to be updated */
+const firebaseConfig = {
+  apiKey: "AIzaSyDaPtQoxz3aDTgLyC3m2iY1ODD2TZzLfo4",
+  authDomain: "sirenesongapp.firebaseapp.com",
+  databaseURL: "https://sirenesongapp.firebaseio.com",
+  projectId: "sirenesongapp",
+  storageBucket: "sirenesongapp.appspot.com",
+  messagingSenderId: "691616626289",
+  appId: "1:691616626289:web:a6bf28b557bb7f3978cdd0",
+  measurementId: "G-P7QSR9X1B0"
+};
+// Initialize firebase application
+firebase.initializeApp(firebaseConfig);
 
-// Add data with POST method using the firestore ADD function.
+/** Endpoint for adding posts using the firestore ADD function. */
 app.post("/addPosts", (request, response) => {
   // Username and message. Time will be automatically added
-  let postSchema = {
+  const postSchema = {
     message: request.body.message,
     userHandle: request.body.userHandle,
     time: new Date().toISOString()
   };
-  //
+  // Using the postSchema, post data into the db
   db.collection("Posts")
     .add(postSchema)
     .then(reference =>
@@ -32,27 +47,104 @@ app.post("/addPosts", (request, response) => {
     });
 });
 
-// Read the data in the database with GET method with Firestore Cloud functions
+/** Endpoint for retrieving all posts in the db. */
 app.get("/getPosts", (request, response) => {
+  // "Posts" is the name of the database (collection in firebase)
+  // Get all the data from the noSql db and store them in an array. Return json object
   db.collection("Posts")
     .orderBy("time", "desc")
     .get()
     .then(results => {
-      let posts = [];
+      const posts = [];
       // result.data() is a json object
       results.forEach(result => {
         posts.push({
           postID: result.id,
-        message: result.data().message,
-        userHandle: result.data().userHandle,
-        time: result.data().time
-        }
-        );
+          message: result.data().message,
+          userHandle: result.data().userHandle,
+          time: result.data().time
+        });
       });
       return response.status(200).json(posts);
     })
     .catch(error => {
       console.log("Error getting documents", error);
+    });
+});
+
+/** User registration with Json web token*/
+app.post("/signup", (request, response) => {
+  // schema for user registration
+  const userSchema = {
+    email: request.body.email,
+    password: request.body.password,
+    confirmPassword: request.body.confirmPassword,
+    handle: request.body.handle
+  };
+
+  // Variables used later
+  let userId;
+  let token;
+  //
+  db.doc(`/Users/${userSchema.handle}`)
+    .get()
+    // Check if the requested handle is taken
+    // If not firebase creates the user
+    .then(documentPath => {
+      if (documentPath.exists) {
+        return response.status(403).json({ handle: "Handle already taken." });
+      } else {
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(
+            userSchema.email,
+            userSchema.password
+          );
+      }
+    })
+    //
+    // Return a Json Web Token to identify the user to firebase services
+    // Return the current token if not expired
+    .then(createdUser => {
+      userId = createdUser.user.uid;
+      return createdUser.user.getIdToken();
+    })
+    // Create a document with the user infomation and the handle as
+    // unique identifier
+    .then(JWT => {
+      token = JWT;
+      const userInfo = {
+        userId,
+        email: userSchema.email,
+        handle: userSchema.handle,
+        time: new Date().toISOString()
+      };
+      return db.doc(`/Users/${userSchema.handle}`).set(userInfo);
+    })
+    // Resolves the promise and send the token
+    .then(() => response.status(201).json({ token }))
+    .catch(error => {
+      // cloud function has special error codes, checks automatically if email is taken
+      console.log(error.code);
+      console.log(error.message);
+      response.status(500).json({ error: error.code });
+    });
+});
+
+app.post("/signin", (response, request) => {
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(request.body.email, request.body.password)
+    .then(result =>
+      response
+        .status(200)
+        .json({ message: `User ${result.user.uid} has signed in.` })
+    )
+    .catch(error => {
+      // cloud function has special error codes, checks automatically if email is taken
+      console.log(error.code);
+      console.log(error.message);
+      response.status(500).json({ error: error.code });
     });
 });
 
